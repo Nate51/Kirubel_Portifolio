a0395ed8e57c7516c71d376a31e214382adc5a73
Nathan Assefa <nathanassefa2003@gmail.com>
2023-10-27 20:22:33 -0900

    This includes the home page and the about us page is halfway done

---
+++ script.js
@@ -0,0 +1,19 @@
+let underline = document.querySelectorAll(".underline-effect");
+
+// The underline effect for the navbar so when you click it, it stays on a single <a>
+underline.forEach(underLine=>{
+    underLine.addEventListener('click', function (e){
+    e.preventDefault();
+    underline.forEach(underLine =>{
+        underLine.classList.remove('effect');
+    });
+    this.classList.add('effect');
+    });
+});
+
+//
+header3=document.getElementById("menu_btn");
+header3.onclick = function(){
+    navBar=document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+}
